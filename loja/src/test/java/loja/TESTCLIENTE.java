7c4eac7d626e7613ef3db0d272b843393b0770ae
11724957 <11724957@ESTB8LI0620.animaedu.intranet>
2018-03-15 22:27:04 -0300

    enviando primeiros arquivos

---
+++ loja/src/test/java/loja/TESTCLIENTE.java
@@ -0,0 +1,8 @@
+package loja;
+
+import java.util.Date;
+
+public class TESTCLIENTE {
+//	Cliente c = new Cliente(1,"Gabriel","garbel","senha","garbelg","11111111111","11 1 11111111", "gabriel@hotmail.com",new Date(),new Date());
+
+}
