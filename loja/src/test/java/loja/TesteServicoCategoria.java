345578b18149ae5ca25b74a502d9ee215b046f49
11724957 <11724957@ESTB8LI0620.animaedu.intranet>
2018-05-16 21:53:18 -0300

    LAB 4 FUNCIONANDO DIREITINHO

---
+++ loja/src/test/java/loja/TesteServicoCategoria.java
@@ -0,0 +1,103 @@
+package loja;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import br.unibh.loja.entidades.Categoria;
+import br.unibh.loja.entidades.Cliente;
+import br.unibh.loja.entidades.Produto;
+import br.unibh.loja.negocio.DAO;
+import br.unibh.loja.negocio.ServicoCategoria;
+import br.unibh.loja.negocio.ServicoCliente;
+import br.unibh.loja.negocio.ServicoProduto;
+import br.unibh.loja.util.Resources;
+
+@RunWith(Arquillian.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TesteServicoCategoria {
+	@Deployment
+	public static Archive<?> createTestArchive() {
+		// Cria o pacote que vai ser instalado no Wildfly para realizacao dos testes
+		return ShrinkWrap.create(WebArchive.class, "testeseguro.war")
+				.addClasses(Categoria.class, Cliente.class, Produto.class, Resources.class, DAO.class,
+						ServicoCategoria.class, ServicoCliente.class, ServicoProduto.class)
+				.addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+	// Realiza as injecoes com CDI
+	@Inject
+	private Logger log;
+	@Inject
+	private ServicoCategoria ss;
+
+	@Test
+	public void teste01_inserirSemErro() throws Exception {
+		log.info("============> Iniciando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		Categoria o = new Categoria(1L, "lavabo");
+		ss.insert(o);
+		Categoria aux = (Categoria) ss.findByName("lavabo").get(0);
+		assertNotNull(aux);
+		log.info("============> Finalizando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	@Test
+	public void teste02_inserirComErro() throws Exception {
+	log.info("============> Iniciando o teste " +
+	Thread.currentThread().getStackTrace()[1].getMethodName());
+	try {
+		Categoria o = new Categoria(1L, "lavabo++++++++-+-");
+	ss.insert(o);
+	} catch (Exception e){
+	assertTrue(checkString(e, "Caracteres permitidos: letras, espaços, ponto e aspas simples"));
+	}
+	log.info("============> Finalizando o teste " +
+	Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	@Test
+	public void teste03_atualizar() throws Exception {
+		log.info("============> Iniciando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		Categoria o = (Categoria) ss.findByName("lavabo").get(0);
+		o.setDescricao("lavabo e sujabo");
+		ss.update(o);
+		Categoria aux = (Categoria) ss.findByName("lavabo e sujabo").get(0);
+		assertNotNull(aux);
+		log.info("============> Finalizando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	@Test
+	public void teste04_excluir() throws Exception {
+		log.info("============> Iniciando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		Categoria o = (Categoria) ss.findByName("lavabo e sujabo").get(0);
+		
+		ss.delete(o);
+		assertEquals(0, ss.findByName("lavabo e sujabo").size());
+		log.info("============> Finalizando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	private boolean checkString(Throwable e, String str) {
+		if (e.getMessage().contains(str)) {
+			return true;
+		} else if (e.getCause() != null) {
+			return checkString(e.getCause(), str);
+		}
+		return false;
+	}
+}
---
+++ loja/src/test/java/loja/TesteServicoCliente.java
@@ -0,0 +1,105 @@
+package loja;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import br.unibh.loja.entidades.Categoria;
+import br.unibh.loja.entidades.Cliente;
+import br.unibh.loja.entidades.Produto;
+import br.unibh.loja.negocio.DAO;
+import br.unibh.loja.negocio.ServicoCategoria;
+import br.unibh.loja.negocio.ServicoCliente;
+import br.unibh.loja.negocio.ServicoProduto;
+import br.unibh.loja.util.Resources;
+
+@RunWith(Arquillian.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TesteServicoCliente {
+	@Deployment
+	public static Archive<?> createTestArchive() {
+		// Cria o pacote que vai ser instalado no Wildfly para realizacao dos testes
+		return ShrinkWrap.create(WebArchive.class, "testeseguro.war")
+				.addClasses(Categoria.class, Cliente.class, Produto.class, Resources.class, DAO.class,
+						ServicoCategoria.class, ServicoCliente.class, ServicoProduto.class)
+				.addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+	// Realiza as injecoes com CDI
+	@Inject
+	private Logger log;
+	@Inject
+	private ServicoCliente ss;
+
+	@Test
+	public void teste01_inserirSemErro() throws Exception {
+		log.info("============> Iniciando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		Cliente o = new Cliente(1L, "Gabriel", "garbelasdf", "senha", "garbelg", "12876581647", "(11)1111-1111",
+				"gabriel@hotmail.com", new Date(), new Date());
+		ss.insert(o);
+		Cliente aux = (Cliente) ss.findByName("Gabriel").get(0);
+		assertNotNull(aux);
+		log.info("============> Finalizando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	@Test
+	public void teste02_inserirComErro() throws Exception {
+	log.info("============> Iniciando o teste " +
+	Thread.currentThread().getStackTrace()[1].getMethodName());
+	try {
+		Cliente o = new Cliente(1L, "Gabriel", "garbel", "senha", "garbelg", "11111111111", "11 1 11111111",
+				"gabriel@hotmail.com", new Date(), new Date());
+	ss.insert(o);
+	} catch (Exception e){
+		assertTrue(checkString(e, "telefone invalido"));
+	}
+	log.info("============> Finalizando o teste " +
+	Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	@Test
+	public void teste03_atualizar() throws Exception {
+		log.info("============> Iniciando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		Cliente o = (Cliente) ss.findByName("Gabriel").get(0);
+		o.setNome("Gabriel G");
+		ss.update(o);
+		Cliente aux = (Cliente) ss.findByName("Gabriel G").get(0);
+		assertNotNull(aux);
+		log.info("============> Finalizando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	@Test
+	public void teste04_excluir() throws Exception {
+		log.info("============> Iniciando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		Cliente o = (Cliente) ss.findByName("Gabriel").get(0);
+		ss.delete(o);
+		assertEquals(0, ss.findByName("Gabriel Gostosao").size());
+		log.info("============> Finalizando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	private boolean checkString(Throwable e, String str) {
+		if (e.getMessage().contains(str)) {
+			return true;
+		} else if (e.getCause() != null) {
+			return checkString(e.getCause(), str);
+		}
+		return false;
+	}
+}
